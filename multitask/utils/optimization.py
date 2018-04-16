64ae51e5f8ec8d016cf9f5a2bd1f699476cfc5ab
janvanrijn <janvanrijn@gmail.com>
2018-04-16 14:52:45 -0400

    WIP modularized optimization

--- multitask/utils/optimization.py
+++ multitask/utils/optimization.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 
-def pack_params(L, Theta_x=None, sigma_l_2=None):
-    if not np.allclose(L, np.tril(L)):
+def pack_params(L=None, Theta_x=None, sigma_l_2=None):
+    if L is not None and not np.allclose(L, np.tril(L)):
         raise ValueError('Matrix L not lower triangular')
 
-    params = L[np.tril_indices(len(L))]
+    params = list()
+    if L is not None:
+        params = L[np.tril_indices(len(L))]
     if Theta_x is not None:
         params = np.append(params, Theta_x)
     if sigma_l_2 is not None:
@@ -13,10 +15,12 @@ def pack_params(L, Theta_x=None, sigma_l_2=None):
     return params
 
 
-def unpack_params(parameters_array, M, include_theta=True, include_sigma=True):
+def unpack_params(parameters_array, M, include_L=True, include_theta=True, include_sigma=True):
     # M = num_tasks, we are required to know this
     expected_size_L = int(M * (M + 1) / 2)
-    expected_size = expected_size_L
+    expected_size = 0
+    if include_L:
+        expected_size += expected_size_L
     if include_theta:
         expected_size += 2
     if include_sigma:
@@ -24,15 +28,24 @@ def unpack_params(parameters_array, M, include_theta=True, include_sigma=True):
 
     if len(parameters_array) != expected_size:
         raise ValueError('Wrong parameter size array. Expected %d got %d' % (expected_size, len(parameters_array)))
-    L = np.zeros((M, M))
-    L[np.tril_indices(M)] = parameters_array[0:expected_size_L]
-    if include_theta:
-        Theta_x = [parameters_array[expected_size_L], parameters_array[expected_size_L+1]]
-    else:
-        Theta_x = [1, 1] # TODO: defaults
 
+    L = None
+    Theta_x = None
+    sigma_l_2 = None
+
+    # L is always first entrees
+    if include_L:
+        L = np.zeros((M, M))
+        L[np.tril_indices(M)] = parameters_array[0:expected_size_L]
+
+        # in this case theta comes after L
+        if include_theta:
+            Theta_x = [parameters_array[expected_size_L], parameters_array[expected_size_L + 1]]
+    elif include_theta:
+        Theta_x = [parameters_array[0], parameters_array[1]]
+
+    # sigma is always last entrees
     if include_sigma:
         sigma_l_2 = parameters_array[len(parameters_array)-M:]
-    else:
-        sigma_l_2 = np.zeros(M)
+
     return L, Theta_x, sigma_l_2
