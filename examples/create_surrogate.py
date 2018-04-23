80738aa36fcebac193ad9fea0db707e6f042a4d0
janvanrijn <janvanrijn@gmail.com>
2018-04-23 16:31:53 -0400

    created surrogates for benchmark

---
+++ examples/create_surrogate.py
@@ -0,0 +1,43 @@
+import argparse
+import csv
+import os
+import pickle
+import sklearn.ensemble
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Proof of concept of Multi-task GP')
+    parser.add_argument('--directory', type=str, default='../data/svm_on_mnist_grid')
+    parser.add_argument('--n_estimators', type=int, default=128)
+    parser.add_argument('--random_state', type=int, default=42)
+
+    return parser.parse_args()
+
+
+def run(args):
+    configs_file = os.path.join(args.directory, 'configs.csv')
+
+    vals_X = list()
+
+    with open(configs_file, 'r') as fp:
+        for row in csv.reader(fp):
+            vals_X.append(row)
+
+    response_values = [('costs.csv', 'rf_cost_surrogate_svm.pkl'), ('fvals.csv', 'rf_surrogate_svm.pkl')]
+
+    for input_file, output_file in response_values:
+        input_path = os.path.join(args.directory, input_file)
+        output_path = os.path.join(args.directory, output_file)
+        vals = list()
+        with open(input_path, 'r') as fp:
+            for row in csv.reader(fp):
+                vals.append(row)
+
+        surrogate = sklearn.ensemble.RandomForestRegressor(n_estimators=args.n_estimators, random_state=args.random_state)
+        surrogate.fit(vals_X, vals)
+        with open(output_path, 'wb') as fp:
+            pickle.dump(surrogate, fp)
+
+
+if __name__ == '__main__':
+    run(parse_args())
