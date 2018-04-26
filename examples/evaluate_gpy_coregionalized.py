091386146aca06870da02634bc35a2773e90f709
janvanrijn <janvanrijn@gmail.com>
2018-04-26 13:59:46 -0400

    made predictions

--- examples/evaluate_gpy_coregionalized.py
+++ examples/evaluate_gpy_coregionalized.py
@@ -14,7 +14,7 @@ def parse_args():
     parser.add_argument('--task_id_column', type=str, default='task_id')
     parser.add_argument('--hyperparameters', type=str, nargs='+', default=None)
     parser.add_argument('--y_column', type=str, default='y')
-    parser.add_argument('--test_size', type=int, default=100)
+    parser.add_argument('--test_size', type=int, default=150)
     parser.add_argument('--num_tasks', type=int, default=5)
     parser.add_argument('--random_seed', type=int, default=42)
     return parser.parse_args()
@@ -71,10 +71,10 @@ def run(args):
     test_indices = np.random.choice(num_obs, args.test_size, replace=False)
     train_indices= np.array(list(set(range(num_obs)) - set(test_indices)))
 
-    task_X_train = tasks_X_values[:,train_indices ,:]
-    task_X_test = tasks_X_values[:,test_indices ,:]
-    task_y_train = tasks_y_values[:,train_indices]
-    task_y_test = tasks_y_values[:,test_indices]
+    task_X_train = tasks_X_values[:, train_indices, :]
+    task_X_test = tasks_X_values[:, test_indices, :]
+    task_y_train = tasks_y_values[:, train_indices, :]
+    task_y_test = tasks_y_values[:, test_indices, :]
 
     # train the model
     kernel = GPy.kern.Matern32(num_feats)
@@ -83,7 +83,14 @@ def run(args):
     # For this kernel, B.kappa encodes the variance now.
     m['.*Mat32.var'].constrain_fixed(1.)
     m.optimize()
-    print(m)
+
+    for idx, task_id in enumerate(tasks):
+        input_index = np.full(task_X_test[idx].shape, idx)
+        output_index = np.full((args.test_size, 1), idx)
+        extended = np.hstack((task_X_test[idx], input_index))
+
+        mean, variance = m.predict(extended, Y_metadata={'output_index': output_index})
+        print(task_id, mean.shape, variance.shape)
 
 
 if __name__ == '__main__':
